57b0a99cfc64e26d9ad7ef2f5830639876e94956
ligermohit07 <mohittatu2007@gmail.com>
2025-04-06 12:08:06 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -104,7 +104,7 @@ async def pub_(bot, message):
     await db.add_frwd(user)
     await send(client, user, "<b>Fᴏʀᴡᴀᴅɪɴɢ sᴛᴀʀᴛᴇᴅ🔥</b>")
     sts.add(time=True)
-    sleep = 1 if _bot['is_bot'] else 10
+    sleep = 0.5 if _bot['is_bot'] else 2
     await msg_edit(m, "<code>processing...</code>") 
     temp.IS_FRWD_CHAT.append(i.TO)
     temp.lock[user] = locked = True
