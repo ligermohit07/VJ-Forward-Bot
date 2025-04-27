650c6a7e9c217013cf26e04f04e9789ff7d55ba6
ligermohit07 <mohittatu2007@gmail.com>
2025-04-27 20:10:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,10 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from os import environ 
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$') 
 
 class Config:
     API_ID = int(environ.get("API_ID", "19822115"))
@@ -12,6 +15,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7404299810"))
+    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002553439091').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
